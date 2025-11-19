167b56612dce249e28d89029328965350a5c42f8
bhbghghbgb <113711814+bhbghghbgb@users.noreply.github.com>
2025-11-19 10:09:34 +0800

    cau hinh frontend unit test bang jest thanh cong

---
+++ New folder/frontend/jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "jest";
+import { createDefaultPreset } from "ts-jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  // setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/index.tsx",
+    "!src/reportWebVitals.ts",
+  ],
+};
+
+export default config;
